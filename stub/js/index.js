73ab9fdf56d0c4fadb44d94cc509647cb4387326
GVRLMAVINASH <gvrlmavinash@gmail.com>
2019-07-13 20:01:13 +0530

    Index page completed
    All coding related Index page is completed

--- stub/js/index.js
+++ stub/js/index.js
@@ -9,8 +9,16 @@ function signup() {
 function closec() {
     document.getElementById('simodal').style.display='none';
     document.getElementById('sumodal').style.display='none';
+    document.getElementById('createnew').style.display='none';
 }
 
 function openSignUpButton(){
     document.getElementById('sumodal').style.display='block';
+}
+function createpost()
+{
+    document.getElementById('createnew').style.display='block';
+}
+function Allposts(){
+    window.location.href= "./html/bloglist.html";
 }
