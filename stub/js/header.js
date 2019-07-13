1be2f00be289c69e6e3b6ee2fe477e949689352a
GVRLMAVINASH <gvrlmavinash@gmail.com>
2019-07-13 19:21:08 +0530

    Index Top Buttons done
    Index top buttons sign in and sign up were Designed and working

---
+++ stub/js/header.js
@@ -0,0 +1,16 @@
+function signin() {
+    document.getElementById('simodal').style.display='block';
+}
+    
+function signup() {
+    document.getElementById('sumodal').style.display='block';
+}
+
+function closec() {
+    document.getElementById('simodal').style.display='none';
+    document.getElementById('sumodal').style.display='none';
+}
+
+function openSignUpButton(){
+    document.getElementById('sumodal').style.display='block';
+}
